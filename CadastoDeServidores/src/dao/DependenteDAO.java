3a1bf4daac283cfc127441885fdffc16024fd092
chosenUndead69 <nohopeleft13@live.com>
2015-11-12 23:47:55 -0200

    Lord Rodrigo criou as classes DependenteDAO e ServidorDAO

---
+++ CadastoDeServidores/src/dao/DependenteDAO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import model.Dependentes;
+
+/**
+ *
+ * @author Lord Devil Hunter
+ */
+public class DependenteDAO {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("trabalho-finalPRO");
+        
+        EntityManager em = emf.createEntityManager();
+        
+        public void begin(){
+            em.getTransaction().begin();
+        }
+        
+        public void commit(){
+            em.getTransaction().commit();
+        }
+        public void rollback(){
+            em.getTransaction().rollback();
+        }
+        
+        public void close(){
+            em.close();
+            emf.close();
+        }
+        
+        public void salvar(Dependentes d){
+            em.persist(d);
+        }
+        public Dependentes pesquisar(long id){
+            return em.find(Dependentes.class, id);
+        }
+        public void apagar(Dependentes d){
+            begin();
+            em.persist(d);
+            commit();
+        }
+        public void atualizar(Dependentes d){
+            begin();
+            em.merge(d);
+            commit();
+        }
+        
+        public List<Dependentes> listar(){
+            List<Dependentes> ls = new ArrayList<Dependentes>();
+            Query query = em.createQuery("select s from Servidor s");
+            return ls = query.getResultList();
+        }
+}
---
+++ CadastoDeServidores/src/dao/EnderecoDAO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import model.Endereco;
+
+/**
+ *
+ * @author Lord Devil Hunter
+ */
+public class EnderecoDAO {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("trabalho-finalPRO");
+        
+        EntityManager em = emf.createEntityManager();
+        
+        public void begin(){
+            em.getTransaction().begin();
+        }
+        
+        public void commit(){
+            em.getTransaction().commit();
+        }
+        public void rollback(){
+            em.getTransaction().rollback();
+        }
+        
+        public void close(){
+            em.close();
+            emf.close();
+        }
+        
+        public void salvar(Endereco e){
+            em.persist(e);
+        }
+        public Endereco pesquisar(long id){
+            return em.find(Endereco.class, id);
+        }
+        public void apagar(Endereco e){
+            begin();
+            em.persist(e);
+            commit();
+        }
+        public void atualizar(Endereco e){
+            begin();
+            em.merge(e);
+            commit();
+        }
+        
+        public List<Endereco> listar(){
+            List<Endereco> ls = new ArrayList<Endereco>();
+            Query query = em.createQuery("select s from Servidor s");
+            return ls = query.getResultList();
+        }
+}
